df129aae2570bd87837b85fd97955cb1fbc2c07f
prilya <36910661+prilya@users.noreply.github.com>
2018-09-24 11:33:19 +0700

    Add files via upload

---
+++ DemoIncrementDecrement.java
@@ -0,0 +1,24 @@
+public class DemoIncrementDecrement{
+    public static void main(String[] args){
+        int x=1, y=1, z=1;
+
+        //nilai awal
+        System.out.println("Nilai x : " +x);
+        System.out.println("Nilai y : " +y);
+        System.out.println("Nilai z : " +z);
+
+        //Incerement
+        x++; y++;
+        System.out.println("Nilai x : " + x);
+        System.out.println("Nilai y : " + y++);
+        System.out.println("Nilai z : " + ++z);
+        System.out.println("Nilai y : " + y);
+
+        //Decrement
+        x--; y--;
+        System.out.println("Nilai x : " + x);
+        System.out.println("Nilai y : " + y--);
+        System.out.println("Nilai z : " + --z);
+        System.out.println("Nilai y : " + y);
+    }
+}
---
+++ TestOr2.java
@@ -0,0 +1,12 @@
+public class TestOr2{
+
+    public static void main(String[] args){
+        int i = 0;
+        int j = 10;
+        boolean test=true;
+        //demonstrasi ||
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(test);
+    }
+}
